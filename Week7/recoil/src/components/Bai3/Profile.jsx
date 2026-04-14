796abd8a2bed83bb6b344fb46e060ec797901bed
DuongThienAn <anpro1608@gmail.com>
2026-04-14 21:52:37 +0700

    Tuan 7

---
+++ Week7/recoil/src/components/Bai3/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useRecoilState } from 'recoil'
+import { userAtom } from '../../state/Bai3/UserAtom'
+
+export default function Profile() {
+    const [user, setUser] = useRecoilState(userAtom);
+
+    function login() {
+        setUser({ userName: 'user' });
+    }
+
+    function logout() {
+        setUser(null);
+    }
+
+    return (
+        <div>
+            {user ? (
+                <div>
+                    <span>{user.userName}</span>
+                    <div> <button onClick={logout}>Đăng xuất</button></div>
+                   
+                </div>
+            ) : (
+                <button onClick={login}>Đăng nhập</button>
+            )}
+        </div>
+    )
+}
